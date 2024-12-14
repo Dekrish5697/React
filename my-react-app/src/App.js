e31a96d47c99d82270f4b225edfd0aff040f36d3
dekrish5612 <dekrish5697@gmail.com>
2024-12-14 21:00:04 +0530

    Initial commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,32 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-function App() {
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/" exact element={<Home/>} />
-                    <Route path="/about" element={<About/>} />
-                    <Route path="/contact" component={<Contact/>} />
-                </Switch>
-            </div>
-        </Router>
-    );
+
+function About() {
+return <h2>About Page</h2>;
 }
-export default App;
+export default About;
