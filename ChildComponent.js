98fd200de658b20626c4d3b50b2d21c19ca45da0
Dekrish5697 <dekrish5697@gmail.com>
2024-12-24 21:34:15 +0530

    Delete ChildComponent.js

--- ChildComponent.js
+++
@@ -1,7 +0,0 @@
-function ChildComponent({ sendMessageToParent }) {
-    const handleClick = () => {
-    sendMessageToParent("Hello from the child!");
-    };
-    return <button onClick={handleClick}>Send Message</button>;
-    }
-    export default ChildComponent;
